b873fb5bfc054381f9377ba2a7f4d41a0097e8e5
MIYAGI Hikaru <hikaru.devel@js4.in>
2018-03-10 13:23:25 +0900

    スマホで表示が全く出来なくなるバグを直す

--- app/javascript/mastodon/features/ui/components/columns_area.js
+++ app/javascript/mastodon/features/ui/components/columns_area.js
@@ -157,9 +157,7 @@ export default class ColumnsArea extends ImmutablePureComponent {
     if (singleColumn) {
       const floatingActionButton = this.context.router.history.location.pathname === '/statuses/new' ? null : <Link key='floating-action-button' to='/statuses/new' className='floating-action-button'><i className='fa fa-pencil' /></Link>;
 
-      return columnIndex !== -1 ? [
-        floatingActionButton,
-      ] : [
+      return [
         <div className='columns-area'>{children}</div>,
 
         floatingActionButton,
